51c86cfc7ff529836629cb631647fab33f563f6b
Ryan-B-Runo <43120875+Ryan-B-Runo@users.noreply.github.com>
2025-07-23 11:34:41 -0500

    Create outline

---
+++ Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+//    static boolean canCreateNote(String note, String[] articles){
+//
+//    }
+
+}
